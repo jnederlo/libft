39d790634350cc34be1fcc10d1f07080324fb16f
Jarvis Nederlof <jarvisnederlof@gmail.com>
2017-03-05 21:29:36 -0800

    commiting new functions

---
+++ libft/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnederlo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/05 15:41:56 by jnederlo          #+#    #+#             */
+/*   Updated: 2017/03/05 17:52:30 by jnederlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	int		size;
+	char	*s2;
+
+	i = 0;
+	size = ft_strlen(s1);
+	s2 = (char*)malloc(size * sizeof(char) + 1);
+	if (s2 == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = 0;
+	return (s2);
+}	
+
